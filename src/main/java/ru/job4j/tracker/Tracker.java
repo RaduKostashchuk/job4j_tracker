a2ab51a6dc1aaef6de99db704876452f56f9d45c
RaduKostashchuk <kostasc@mail.ru>
2021-08-12 12:55:03 +0700

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -46,7 +46,8 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        boolean result = index != -1 && item != null;
+        System.out.println(index);
+        boolean result = index != -1;
         if (result) {
             item.setId(id);
             items[index] = item;
