b8114cd861c646ca7eedd9d02b3e7af39844fab1
RaduKostashchuk <kostasc@mail.ru>
2021-08-12 09:22:40 +0700

    5. Tracker - хранилище [#396 #188561]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,14 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[items.length];
-        int count = 0;
-        for (int index = 0; index < size; index++) {
-            if (items[index] != null) {
-                result[count++] = items[index];
-            }
-        }
-        return Arrays.copyOf(result, count);
+        return Arrays.copyOf(items, size);
     }
 
     public Item findById(int id) {
