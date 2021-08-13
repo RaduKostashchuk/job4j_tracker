243d6a31a5cc45f9c364782e82a6e9d2de22bde6
RaduKostashchuk <kostasc@mail.ru>
2021-08-13 10:02:50 +0700

    4.1. Разрыв зависимости StartUI от Scanner. [#181778]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,14 +2,13 @@ package ru.job4j.tracker;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class Item {
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-    private static final DateTimeFormatter FORMATTER =
-            DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
