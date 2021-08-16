f95d70db76a087f12b9061adf9d3a9fc10f711e2
RaduKostashchuk <kostasc@mail.ru>
2021-08-16 17:12:34 +0700

    4. final [#334073 #190059]

--- src/main/java/ru/job4j/examen/PlayStatic.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.examen;
-
-public class PlayStatic {
-    public static void main(String[] args) {
-        Object obj = new Object();
-        System.out.println(obj.toString());
-    }
-}
