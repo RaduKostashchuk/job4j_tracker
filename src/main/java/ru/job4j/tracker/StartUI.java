7b761755fe8e57805f3bea482b9df58960be1c19
RaduKostashchuk <kostasc@mail.ru>
2021-08-20 17:16:42 +0700

    6. Изменить программу Tracker из 2-го модуля [#10039 #192183]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -36,14 +36,10 @@ public class StartUI {
         Output out = new ConsoleOutput();
         Input input = new ValidateInput(new ConsoleInput(), out);
         Tracker tracker = new Tracker();
-        List<UserAction> list = new ArrayList<>();
-        list.add(new CreateAction(out));
-        list.add(new ShowAllAction(out));
-        list.add(new ReplaceAction(out));
-        list.add(new DeleteAction(out));
-        list.add(new ShowByIdAction(out));
-        list.add(new ShowByNameAction(out));
-        list.add(new ExitAction());
+        List<UserAction> list = List.of(
+                new CreateAction(out), new ShowAllAction(out), new ReplaceAction(out),
+                new DeleteAction(out), new ShowByIdAction(out),
+                new ShowByNameAction(out), new ExitAction());
         new StartUI(out).init(input, tracker, list);
     }
 }
