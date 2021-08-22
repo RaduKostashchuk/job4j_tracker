314996b9aeee85042655a7a6437152c116d00843
RaduKostashchuk <kostasc@mail.ru>
2021-08-22 21:18:42 +0700

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,124 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    @Test
+    public void whenAscNames() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 2));
+        list.add(new Job("Impl task", 3));
+        Comparator<Job> comp = new SortJobNameAsc();
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Fix bug", 2));
+        expected.add(new Job("Impl task", 3));
+        expected.add(new Job("Replace HDD", 1));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenDescNames() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 2));
+        list.add(new Job("Impl task", 3));
+        Comparator<Job> comp = new SortJobNameDesc();
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Replace HDD", 1));
+        expected.add(new Job("Impl task", 3));
+        expected.add(new Job("Fix bug", 2));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenAscPrio() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Impl task", 3));
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 2));
+        Comparator<Job> comp = new SortJobPrioAsc();
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Replace HDD", 1));
+        expected.add(new Job("Fix bug", 2));
+        expected.add(new Job("Impl task", 3));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenDescPrio() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Impl task", 3));
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 2));
+        Comparator<Job> comp = new SortJobPrioDesc();
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Impl task", 3));
+        expected.add(new Job("Fix bug", 2));
+        expected.add(new Job("Replace HDD", 1));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenAscNameAscPrio() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Impl task", 3));
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 3));
+        list.add(new Job("Fix bug", 2));
+        Comparator<Job> comp = new SortJobNameAsc().thenComparing(new SortJobPrioAsc());
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Fix bug", 2));
+        expected.add(new Job("Fix bug", 3));
+        expected.add(new Job("Impl task", 3));
+        expected.add(new Job("Replace HDD", 1));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenDescNameDescPrio() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Impl task", 3));
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 3));
+        list.add(new Job("Fix bug", 2));
+        Comparator<Job> comp = new SortJobNameDesc().thenComparing(new SortJobPrioDesc());
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Replace HDD", 1));
+        expected.add(new Job("Impl task", 3));
+        expected.add(new Job("Fix bug", 3));
+        expected.add(new Job("Fix bug", 2));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenDescNameAscPrio() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Impl task", 3));
+        list.add(new Job("Replace HDD", 1));
+        list.add(new Job("Fix bug", 3));
+        list.add(new Job("Fix bug", 2));
+        Comparator<Job> comp = new SortJobNameDesc().thenComparing(new SortJobPrioAsc());
+        list.sort(comp);
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Replace HDD", 1));
+        expected.add(new Job("Impl task", 3));
+        expected.add(new Job("Fix bug", 2));
+        expected.add(new Job("Fix bug", 3));
+        assertEquals(expected, list);
+    }
+}
