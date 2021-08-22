fcb34fe9c2bcee61994ce6976ec6e46f70491fc0
RaduKostashchuk <kostasc@mail.ru>
2021-08-22 23:18:07 +0700

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int length = Math.min(left.length(), right.length());
+        for (int index = 0; index < length; index++) {
+            if (left.charAt(index) < right.charAt(index)) {
+                return -1;
+            }
+            if (left.charAt(index) > right.charAt(index)) {
+                return 1;
+            }
+        }
+        return Integer.compare(left.length(), right.length());
+    }
+}
