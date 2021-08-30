061d0d5f5c68d479f8b09e43a99f8b0779932a41
RaduKostashchuk <kostasc@mail.ru>
2021-08-30 09:33:05 +0700

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -8,7 +8,9 @@ public class StreamUsage {
     public static void main(String[] args) {
         List<Integer> source = new ArrayList<>(
                 Arrays.asList(1, 20, 45, -8, 29, -4, 78, 419, -76, 3));
-        List<Integer> result = source.stream().filter(n -> n > 0).toList();
+        List<Integer> result = source.stream()
+                .filter(n -> n > 0)
+                .toList();
         System.out.println(source);
         System.out.println(result);
     }
